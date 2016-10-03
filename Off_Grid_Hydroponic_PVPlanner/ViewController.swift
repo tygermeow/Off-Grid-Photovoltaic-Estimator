1ed54b0fe0d306a716bf75c87cf8e84dd13aebff
Matsu Thornton <matsut@hawaii.edu>
2016-10-02 17:03:37 -1000

    First working version of the calculator. Testing my commit to version control now.

--- Off_Grid_Hydroponic_PVPlanner/ViewController.swift
+++ Off_Grid_Hydroponic_PVPlanner/ViewController.swift
@@ -8,8 +8,19 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
 
+
+class ViewController: NSViewController {
+//MARK: properties
+    
+    @IBOutlet weak var cont_load_text_field: NSTextField!
+
+    @IBOutlet weak var sun_hours_text_field: NSTextField!
+    @IBOutlet weak var min_pv_size_label: NSTextField!
+    @IBOutlet weak var battery_size_label: NSTextField!
+    @IBOutlet weak var batt12: NSTextField!
+    @IBOutlet weak var batt24: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +31,33 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+ 
+    @IBAction func contload_change(_ sender: NSTextField) {
+        let contload = atof(cont_load_text_field.stringValue)
+        let sunhours = atof(sun_hours_text_field.stringValue)
+        let minpvsize = (contload*24)/(sunhours*0.7)
+        let battsize = contload*(24-sunhours)*2
+        let batt12_calc = battsize/12
+        let batt24_calc = battsize/24
+        min_pv_size_label.stringValue = String(format: "%.2f",minpvsize)
+        battery_size_label.stringValue = String(format: "%.2f",battsize)
+        batt12.stringValue = String(format: "%.2f",batt12_calc)
+        batt24.stringValue = String(format: "%.2f",batt24_calc)
+
+    }
+
+    @IBAction func calculate(_ sender: NSButtonCell) {
+        let contload = atof(cont_load_text_field.stringValue)
+        let sunhours = atof(sun_hours_text_field.stringValue)
+        let minpvsize = (contload*24)/(sunhours*0.7)
+        let battsize = contload*(24-sunhours)*2
+        let batt12_calc = battsize/12
+        let batt24_calc = battsize/24
+        min_pv_size_label.stringValue = String(format: "%.2f",minpvsize)
+        battery_size_label.stringValue = String(format: "%.2f",battsize)
+        batt12.stringValue = String(format: "%.2f",batt12_calc)
+        batt24.stringValue = String(format: "%.2f",batt24_calc)
     }
 
 
